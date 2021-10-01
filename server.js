2c5102e841c2cb05f4656ffa48f7c651bd91b8f2
ike-Njoku <ikenjokudc@gmail.com>
2021-10-01 18:13:21 +0100

    Server bug fixes

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const path = require('path');
 const ssl = require('force-ssl-heroku');
 const PORT = process.env.PORT;
 const app = express();
-app.unsubscribe(ssl);
-
+app.use(ssl);
 app.use(express.static(__dirname + '/dist/crave'));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname +
