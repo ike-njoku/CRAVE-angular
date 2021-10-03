2288d183d66cfbe3b1dba2f2e8a7aea156f72dd5
ike-Njoku <ikenjokudc@gmail.com>
2021-10-03 12:56:17 +0100

    routing updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'index', loadChildren: () => import('./index/index.module').then(m => m.IndexModule) }];
+const routes: Routes = [
+  {path: '', redirectTo:'index', pathMatch: 'full'},
+  { path: 'index', loadChildren: () => import('./index/index.module').then(m => m.IndexModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
