edf16bd46816dce441bbca8d33944f4db5a80f44
ike-Njoku <ikenjokudc@gmail.com>
2021-10-25 12:33:38 +0100

    updates

--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -1,13 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { CreateEnumeratorDto } from '../enumerator/create-enumerator/create-enumerator-dto';
+import { EnumeratorSignInDto } from './enumerator-sign-in-dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  signIn() {
+  enumeratorSignIn(enumerator: EnumeratorSignInDto): Observable<any> {
+    const subUrl = '';
+    return this.http.post<CreateEnumeratorDto>(`${environment.baseUrl}/${subUrl}`, enumerator)
+      .pipe(
+        (catchError(this.handleError))
+      )
+  }
 
+  private handleError(error: any) {
+    console.log(error, 'color: darkblue')
+    return throwError('Could not Sign in at this moment')
   }
 }
