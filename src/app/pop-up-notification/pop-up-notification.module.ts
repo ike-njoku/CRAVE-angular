b1071409b48ac3caf44a5a115cfd00e8d2b30674
ike-Njoku <ikenjokudc@gmail.com>
2021-11-07 12:01:49 +0100

    Updates

---
+++ src/app/pop-up-notification/pop-up-notification.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PopUpNotificationComponent } from './pop-up-notification/pop-up-notification.component';
+
+
+
+@NgModule({
+  declarations: [
+    PopUpNotificationComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PopUpNotificationModule { }
---
+++ src/app/pop-up-notification/pop-up-notification/pop-up-notification.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pop-up-notification',
+  templateUrl: './pop-up-notification.component.html',
+  styleUrls: ['./pop-up-notification.component.css']
+})
+export class PopUpNotificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
